59eae9454436d799eb55f1ac7b78a32240a2375f
Tushar Kathpal <tusharkathpalia@gmail.com>
2025-04-20 19:14:37 +0530

    Add Database and Warehouse

---
+++ main.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    snowflake = {
+      source  = "Snowflakedb/snowflake"
+      version = "1.0.5"
+    }
+  }
+}
+
+provider "snowflake" {
+  role = "SYSADMIN"
+}
+
+resource "snowflake_database" "db" {
+  name = "TF_DEMO"
+}
+
+resource "snowflake_warehouse" "warehouse" {
+  name           = "TF_DEMO"
+  warehouse_size = "xsmall"
+  auto_suspend   = 60
+}
